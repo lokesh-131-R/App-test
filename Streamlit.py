2035378680788148ecc7d338d19c016130ed01b2
Lokesh R <115632121+lokesh-131-R@users.noreply.github.com>
2024-12-09 18:54:13 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,2 +1,10 @@
 import streamlit as st
+
+if "authenticated" not in st.session_state:
+    st.session_state["authenticated"] = False
+
+def authenticate(username, password):
+    # Replace these with your desired username and password
+    return username == "username" and password == "password"
+  
 st.write("Hello World")
